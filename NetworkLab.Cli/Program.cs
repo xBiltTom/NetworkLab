91ceffe4bed3a67bcb5368d3b45801c3e32da9b5
xBiltTom <biltonjr@hotmail.com>
2026-06-28 09:54:50 -0500

    feat: Modelo de IPv4

--- NetworkLab.Cli/Program.cs
+++ NetworkLab.Cli/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using NetworkLab.Core.Models;
+
+var ip = new IPv4Address(192, 168, 1, 34);
+
+Console.WriteLine(ip.Octet1);
+Console.WriteLine(ip.Octet2);
+Console.WriteLine(ip.Octet3);
+Console.WriteLine(ip.Octet4);
---
+++ NetworkLab.Core/Models/IPv4Address.cs
@@ -0,0 +1,17 @@
+namespace NetworkLab.Core.Models;
+
+public class IPv4Address{
+    // Definimos propiedades (es como declarar la variable y crear un método get, pero en csharp se crean los 2 en 1)
+    public byte Octet1 { get;}
+    public byte Octet2 { get;}
+    public byte Octet3 { get;}
+    public byte Octet4 { get;}
+
+    public IPv4Address(byte octet1, byte octet2, byte octet3, byte octet4)
+    {
+        Octet1 = octet1;
+        Octet2 = octet2;
+        Octet3 = octet3;
+        Octet4 = octet4;
+    }
+}
